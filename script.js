bf66f08d8d4a4693d80f6e71e527d7e4a1b8d629
helgart007 <helg.ua.ukr@gmail.com>
2015-12-24 18:48:07 +0200

    iHelper 0.3
    add timesheet enhancements

--- script.js
+++ script.js
@@ -28,6 +28,14 @@ function UpdateRow(row, data, taskId) {
 	$(row).append(rowHtml);
 }
 
+function DateFormat(date) {
+	return date.substr(3,3) + date.substr(0,3) + date.substr(6)
+}
+
+function Pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
+
 $(document).ready(function() {
 	var table = TryGetTargetTable();
 	if (table)
@@ -41,4 +49,23 @@ $(document).ready(function() {
 				});
 			}		
 		})
+	// add time adjustment
+	$(document).on('DOMNodeInserted', '.fancybox-type-iframe iframe', function() {
+		var iframe = $('.fancybox-type-iframe iframe');
+		$(iframe).load(function() {
+			$('#JS_EFID231', iframe.contents()).val(24);
+			$('#JS_EFID229', iframe.contents()).val('Development.');
+			if ($('#JS_EFID233', iframe.contents()))
+				$('input[type="button"][value="Not chargeable"]', iframe.contents()).click();
+			$('#JS_EFID232', iframe.contents()).focus();
+			$('#formfieldcontainer-f230', iframe.contents()).append('<input id="yesterday" type="button" value="Yestarday" style="margin-left:10px">');
+			$('#yesterday', iframe.contents()).click(function() {
+				var dateInput = $('#JS_EFID230', iframe.contents());
+				var date = new Date(DateFormat(dateInput.val()));
+				date.setDate(date.getDate() - 1);
+				dateInput.val(Pad(date.getDate()) + '-' + Pad(date.getMonth() + 1) + '-' + date.getFullYear());
+				$('#JS_EFID232', iframe.contents()).focus();
+			});
+		});
+	});
 });
