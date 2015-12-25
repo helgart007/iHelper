51980b79c723660e3d9049a9c99a5d561df72fea
helgart007 <helg.ua.ukr@gmail.com>
2015-12-25 13:17:17 +0200

    iHelper 0.7.5
    fixed issues caused by interleave update, other fixes

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ function GetAbbreviation(str) {
 	return str.match(/\b([A-Z])/g).join('');
 }
 
+function FormattedFloat(f) {
+    return parseFloat(f.toFixed(2));
+}
+
 function UpdateRow(row, estdata, taskId, statistics) {
 	var rowHtml = '<td class="estimation-cell">';
 	var estimationItems = $('table.crmclick tbody tr[onclick]', estdata);
@@ -109,8 +113,10 @@ function UpdateRowHours(row, timedata) {
 }
 
 function DateFormat(date) {
-	if (date != null)
-		return date.substr(3,3) + date.substr(0,3) + date.substr(6)
+	if (date != null) {
+       var month = parseInt(date.substr(3,2));
+	   return date.substr(3,2) + "-" + date.substr(0,3) + (month > 9 ? "15" : "16");
+    }
 }
 
 function Pad(n) {
@@ -150,13 +156,13 @@ function UpdateListStatistics(statistics) {
 			totalManHours += hours;
 			timeInfoHtml += '<tr>' +
 				 '<td>' + work + '</td>' +
-				 '<td>' + hours + '</td>' +
+				 '<td>' + FormattedFloat(hours) + '</td>' +
 				 '</tr>';
 		}
 		timeInfoHtml = '<div class="time-info"><table>'
-			+ '<tr class="time-info-total"><td>Total</td><td>' + totalManHours + '</td></tr>' + timeInfoHtml
+			+ '<tr class="time-info-total"><td>Total</td><td>' + FormattedFloat(totalManHours) + '</td></tr>' + timeInfoHtml
 			+ '</table></div>';
-		table += '<a href="javascript:void(0)">' + totalManHours + '</a>' + timeInfoHtml + '</td>';
+		table += '<a href="javascript:void(0)">' + FormattedFloat(totalManHours) + '</a>' + timeInfoHtml + '</td>';
 	}
 	table += '</div>';
 	$('table.crm').parent().append(table);
@@ -176,13 +182,13 @@ function InitAddTimeAgjustments() {
 	$(document).on('DOMNodeInserted', '.fancybox-type-iframe iframe', function() {
 		var iframe = $('.fancybox-type-iframe iframe');
 		$(iframe).load(function() {
-			if ($('#JS_EFID229', iframe.contents()).length === 1 && $('input[value="Delete"]', iframe.contents()).length === 0) {
+			if ($('#JS_t1-f229-_new_', iframe.contents()).length === 1 && $('input[value="Delete"]', iframe.contents()).length === 0) {
 				// defaults
-				$('#JS_EFID231', iframe.contents()).val(24);
-				$('#JS_EFID229', iframe.contents()).val('Development.');
-				if ($('#JS_EFID233', iframe.contents()))
+				$('#JS_t1-f231-_new_', iframe.contents()).val(24);
+				$('#JS_t1-f229-_new_', iframe.contents()).val('Development.');
+				if ($('#JS_t1-f233-_new_', iframe.contents()))
 					$('input[type="button"][value="Not chargeable"]', iframe.contents()).click();
-				$('#JS_EFID232', iframe.contents()).focus();
+				$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				// yesterday
 				var date = new Date(), buttonTitle;
 				if (date.getDay() === 1) {
@@ -192,53 +198,53 @@ function InitAddTimeAgjustments() {
 					buttonTitle = 'Yesterday';
 					date.setDate(date.getDate() - 1);
 				}
-				$('#formfieldcontainer-f230', iframe.contents()).append('<input id="reportDateP" type="button" value="' + buttonTitle + '" style="margin-left:10px">');
+				$('#ffc-t1-f230', iframe.contents()).append('<input id="reportDateP" type="button" value="' + buttonTitle + '" style="margin-left:10px">');
 				$('#reportDateP', iframe.contents()).click(function() {
-					var dateInput = $('#JS_EFID230', iframe.contents());
+					var dateInput = $('#JS_t1-f230-_new_', iframe.contents());
 					dateInput.val(Pad(date.getDate()) + '-' + Pad(date.getMonth() + 1) + '-' + date.getFullYear());
-					$('#JS_EFID232', iframe.contents()).focus();
+					$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				});
 				// management
-				$('#formfieldcontainer-f231', iframe.contents()).append('<input id="reportManag" type="button" value="Management" style="margin-left:10px">');
+				$('#ffc-t1-f231', iframe.contents()).append('<input id="reportManag" type="button" value="Management" style="margin-left:10px">');
 				$('#reportManag', iframe.contents()).click(function() {
-					$('#JS_EFID231', iframe.contents()).val(21);
-					$('#JS_EFID229', iframe.contents()).val('Management.');
-					$('#JS_EFID232', iframe.contents()).focus();
+					$('#JS_t1-f231-_new_', iframe.contents()).val(21);
+					$('#JS_t1-f229-_new_', iframe.contents()).val('Management.');
+					$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				});
 				// functional designs
-				$('#formfieldcontainer-f231', iframe.contents()).append('<input id="reportFuncDes" type="button" value="Functional Designs" style="margin-left:10px">');
+				$('#ffc-t1-f231', iframe.contents()).append('<input id="reportFuncDes" type="button" value="Functional Designs" style="margin-left:10px">');
 				$('#reportFuncDes', iframe.contents()).click(function() {
-					$('#JS_EFID231', iframe.contents()).val(24);
-					$('#JS_EFID229', iframe.contents()).val('Functional designs.');
-					$('#JS_EFID232', iframe.contents()).focus();
+					$('#JS_t1-f231-_new_', iframe.contents()).val(24);
+					$('#JS_t1-f229-_new_', iframe.contents()).val('Functional designs.');
+					$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				});
 				// code review
-				$('#formfieldcontainer-f231', iframe.contents()).append('<input id="reportCodeRev" type="button" value="Code Review" style="margin-left:10px">');
+				$('#ffc-t1-f231', iframe.contents()).append('<input id="reportCodeRev" type="button" value="Code Review" style="margin-left:10px">');
 				$('#reportCodeRev', iframe.contents()).click(function() {
-					$('#JS_EFID231', iframe.contents()).val(24);
-					$('#JS_EFID229', iframe.contents()).val('Code review.');
-					$('#JS_EFID232', iframe.contents()).focus();
+					$('#JS_t1-f231-_new_', iframe.contents()).val(24);
+					$('#JS_t1-f229-_new_', iframe.contents()).val('Code review.');
+					$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				});
 				// local testing
-				$('#formfieldcontainer-f231', iframe.contents()).append('<input id="reportTestLocal" type="button" value="Local Testing" style="margin-left:10px">');
+				$('#ffc-t1-f231', iframe.contents()).append('<input id="reportTestLocal" type="button" value="Local Testing" style="margin-left:10px">');
 				$('#reportTestLocal', iframe.contents()).click(function() {
-					$('#JS_EFID231', iframe.contents()).val(40);
-					$('#JS_EFID229', iframe.contents()).val('Local testing.');
-					$('#JS_EFID232', iframe.contents()).focus();
+					$('#JS_t1-f231-_new_', iframe.contents()).val(40);
+					$('#JS_t1-f229-_new_', iframe.contents()).val('Local testing.');
+					$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				});
 				// sandbox testing
-				$('#formfieldcontainer-f231', iframe.contents()).append('<input id="reportTestSand" type="button" value="Sandbox Testing" style="margin-left:10px">');
+				$('#ffc-t1-f231', iframe.contents()).append('<input id="reportTestSand" type="button" value="Sandbox Testing" style="margin-left:10px">');
 				$('#reportTestSand', iframe.contents()).click(function() {
-					$('#JS_EFID231', iframe.contents()).val(40);
-					$('#JS_EFID229', iframe.contents()).val('Sandbox testing.');
-					$('#JS_EFID232', iframe.contents()).focus();
+					$('#JS_t1-f231-_new_', iframe.contents()).val(40);
+					$('#JS_t1-f229-_new_', iframe.contents()).val('Sandbox testing.');
+					$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				});
 				// live testing
-				$('#formfieldcontainer-f231', iframe.contents()).append('<input id="reportTestLive" type="button" value="Live Testing" style="margin-left:10px">');
+				$('#ffc-t1-f231', iframe.contents()).append('<input id="reportTestLive" type="button" value="Live Testing" style="margin-left:10px">');
 				$('#reportTestLive', iframe.contents()).click(function() {
-					$('#JS_EFID231', iframe.contents()).val(40);
-					$('#JS_EFID229', iframe.contents()).val('Live testing.');
-					$('#JS_EFID232', iframe.contents()).focus();
+					$('#JS_t1-f231-_new_', iframe.contents()).val(40);
+					$('#JS_t1-f229-_new_', iframe.contents()).val('Live testing.');
+					$('#JS_t1-f232-_new_', iframe.contents()).focus();
 				});
 			}
 		});
@@ -249,10 +255,10 @@ function InitAddEstimationAgjustments() {
 	$(document).on('DOMNodeInserted', '.fancybox-type-iframe iframe', function() {
 		var iframe = $('.fancybox-type-iframe iframe');
 		$(iframe).load(function() {
-			if ($('#JS_EFID270', iframe.contents()).length === 1 && $('input[value="Delete"]', iframe.contents()).length === 0) {
-				var userId = $('#JS_EFID270', iframe.contents()).val();
+			if ($('#JS_t4-f270-_new_', iframe.contents()).length === 1 && $('input[value="Delete"]', iframe.contents()).length === 0) {
+				var userId = $('#JS_t4-f270-_new_', iframe.contents()).val();
 				
-				$('#formfieldcontainer-f270', iframe.contents()).append(
+				$('#ffc-t4-f270', iframe.contents()).append(
 					'<div>' +
 						'<input id="estFuncDes" type="button" value="Functional Designs" style="margin-right:5px">' +
 						'<input id="estUIDes" type="button" value="UI Designs" style="margin-right:5px">' +
@@ -266,75 +272,75 @@ function InitAddEstimationAgjustments() {
 				);
 				// functional designs				
 				$('#estFuncDes', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(userId);
-					$('#JS_EFID271', iframe.contents()).val('Functional design');
-					$('#JS_EFID272', iframe.contents()).val(2).focus();
-					$('#JS_EFID275', iframe.contents()).val('Functional designs.');
-					$('#JS_EFID276', iframe.contents()).val(10);
+					$('#JS_t4-f270-_new_', iframe.contents()).val(userId);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Functional design');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(2).focus();
+					$('#JS_t4-f275-_new_', iframe.contents()).val('Functional designs.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(10);
 				});
 				
 				// ui designs
 				$('#estUIDes', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(589);
-					$('#JS_EFID271', iframe.contents()).val('Functional design');
-					$('#JS_EFID272', iframe.contents()).val(8).focus();
-					$('#JS_EFID275', iframe.contents()).val('UI designs.');
-					$('#JS_EFID276', iframe.contents()).val(15);
+					$('#JS_t4-f270-_new_', iframe.contents()).val(589);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Functional design');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(8).focus();
+					$('#JS_t4-f275-_new_', iframe.contents()).val('UI designs.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(15);
 				});
 				
 				// frontend development
 				$('#estFrDev', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(513);
-					$('#JS_EFID271', iframe.contents()).val('Development');
-					$('#JS_EFID272', iframe.contents()).val(4).focus();
-					$('#JS_EFID275', iframe.contents()).val('Frontend development.');
-					$('#JS_EFID276', iframe.contents()).val(20);
+					$('#JS_t4-f270-_new_', iframe.contents()).val(513);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Development');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(4).focus();
+					$('#JS_t4-f275-_new_', iframe.contents()).val('Frontend development.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(20);
 				});
 				
 				// development
 				$('#estDev', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(userId).focus();
+					$('#JS_t4-f270-_new_', iframe.contents()).val(userId).focus();
 					//$('#formfieldcontainer-f270 input.search_input', iframe.contents()).focus();
-					$('#JS_EFID271', iframe.contents()).val('Development');
-					$('#JS_EFID272', iframe.contents()).val(24);
-					$('#JS_EFID275', iframe.contents()).val('Development.');
-					$('#JS_EFID276', iframe.contents()).val(30);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Development');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(24);
+					$('#JS_t4-f275-_new_', iframe.contents()).val('Development.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(30);
 				});
 				
 				// code review
 				$('#estCodeRev', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(userId);
-					$('#JS_EFID271', iframe.contents()).val('Code review');
-					$('#JS_EFID272', iframe.contents()).val(2).focus();
-					$('#JS_EFID275', iframe.contents()).val('Code review.');
-					$('#JS_EFID276', iframe.contents()).val(35);
+					$('#JS_t4-f270-_new_', iframe.contents()).val(userId);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Code review');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(2).focus();
+					$('#JS_t4-f275-_new_', iframe.contents()).val('Code review.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(35);
 				});
 				
 				// local testing
 				$('#estLocTest', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(551);
-					$('#JS_EFID271', iframe.contents()).val('Acceptance test');
-					$('#JS_EFID272', iframe.contents()).val(2).focus();
-					$('#JS_EFID275', iframe.contents()).val('Local testing.');
-					$('#JS_EFID276', iframe.contents()).val(40);
+					$('#JS_t4-f270-_new_', iframe.contents()).val(551);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Acceptance test');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(2).focus();
+					$('#JS_t4-f275-_new_', iframe.contents()).val('Local testing.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(40);
 				});
 				
 				// sandbox testing
 				$('#estSandTest', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(551);
-					$('#JS_EFID271', iframe.contents()).val('Acceptance test');
-					$('#JS_EFID272', iframe.contents()).val(2).focus();
-					$('#JS_EFID275', iframe.contents()).val('Sandbox testing.');
-					$('#JS_EFID276', iframe.contents()).val(45);
+					$('#JS_t4-f270-_new_', iframe.contents()).val(551);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Acceptance test');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(2).focus();
+					$('#JS_t4-f275-_new_', iframe.contents()).val('Sandbox testing.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(45);
 				});
 				
 				// live testing
 				$('#estLiveTest', iframe.contents()).click(function() {
-					$('#JS_EFID270', iframe.contents()).val(551);
-					$('#JS_EFID271', iframe.contents()).val('Acceptance test');
-					$('#JS_EFID272', iframe.contents()).val(1).focus();
-					$('#JS_EFID275', iframe.contents()).val('Live testing.');
-					$('#JS_EFID276', iframe.contents()).val(50);
+					$('#JS_t4-f270-_new_', iframe.contents()).val(551);
+					$('#JS_t4-f271-_new_', iframe.contents()).val('Acceptance test');
+					$('#JS_t4-f272-_new_', iframe.contents()).val(1).focus();
+					$('#JS_t4-f275-_new_', iframe.contents()).val('Live testing.');
+					$('#JS_t4-f276-_new_', iframe.contents()).val(50);
 				});
 			}
 		});
@@ -349,39 +355,47 @@ function InitTaskNumberTrim() {
 }
 
 function InitAddTaskAdjustments() {
-	if ($('#navigation a.active[href="edit.php?e=_new_"]').length === 1) {
+	if ($('#navigation a.active[href="record.php?new&table=entity"]').length === 1) {
 		// left values
-		$('#JS_customer').val(577);
-		$('#JS_status').val('2. Toegewezen');
-		$('#JS_EFID237').val('3 Wijziging');
-		$('#JS_EFID227').val('3. Medium');
+		$('#JS_entity-CRMcustomer-_new_').val(577);
+		$('#JS_entity-status-_new_').val('2. Toegewezen');
+		$('#JS_entity-f237-_new_').val('3 Wijziging');
+		$('#JS_entity-f227-_new_').val('3. Medium');
 		// project
-		var project = $('#JS_EFID2');
+		var project = $('#JS_entity-f2-_new_');
 		project.width(94);
 		project.after(
-			'<input id="projCW" type="button" value="CW" style="margin-left:10px">' + 
-			'<input id="projS" type="button" value="S" style="margin-left:5px">' +
+			'<input id="projCW" type="button" value="C" style="margin-left:10px">' + 
+			'<input id="projR" type="button" value="R" style="margin-left:5px">' +
+            '<input id="projH" type="button" value="H" style="margin-left:5px">' +
+            '<input id="projS" type="button" value="S" style="margin-left:5px">' +
 			'<input id="projT" type="button" value="T" style="margin-left:5px">'
 		);
 		$('#projCW').click(function() {
 			project.val(9);
 		});
-		$('#projS').click(function() {
-			project.val(140);
+		$('#projR').click(function() {
+			project.val(152);
+		});
+        $('#projH').click(function() {
+			project.val(158);
+		});
+        $('#projS').click(function() {
+			project.val(155);
 		});
 		$('#projT').click(function() {
 			project.val(100);
 		});
 		// one more value
-		$('#JS_EFID5').val(82);
+		$('#JS_entity-f5-_new_').val(82);
 		// define scripts
 		var devDate, testDate, liveDate, devValue, testValue, liveValue;
 		var currentDate = new Date();
-		$('#JS_EFID4 option').each(function(){			
-			var tDate = $(this).text().match(/T \d{2}-\d{2}-\d{2}/);
+		$('#JS_entity-f4-_new_ option').each(function(){			
+			var tDate = $(this).text().match(/T \d{2}-\d{2}/);
 			if (tDate != null)
 				tDate = new Date(DateFormat(tDate.toString().slice(2)));
-			var lDate = $(this).text().match(/L \d{2}-\d{2}-\d{2}/);
+			var lDate = $(this).text().match(/L \d{2}-\d{2}/);
 			if (lDate != null)
 				lDate = new Date(DateFormat(lDate.toString().slice(2)));
 			if (tDate != null && lDate != null) {
@@ -399,11 +413,11 @@ function InitAddTaskAdjustments() {
 				}
 			}
 		});
-		// console.debug('DEV : ' + $(devValue).text());
-		// console.debug('TEST: ' + $(testValue).text());
-		// console.debug('LIVE: ' + $(liveValue).text());		
+		 console.debug('DEV : ' + $(devValue).text());
+		 console.debug('TEST: ' + $(testValue).text());
+		 console.debug('LIVE: ' + $(liveValue).text());		
 		// sprints
-		var sprint = $('#JS_EFID4');
+		var sprint = $('#JS_entity-f4-_new_');
 		sprint.parent().after(
 			'<div class="newtask-sprints">' +
 			'<input id="spDev" type="button" value="Dev">' +
@@ -425,7 +439,7 @@ function InitAddTaskAdjustments() {
 			sprint.val(130);
 		});
 		//project
-		$('#JS_EFID3').val(53);
+		$('#JS_entity-f3-_new_').val(53);
 	}
 }
 
